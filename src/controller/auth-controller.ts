c3b7c121accabd9d1fff0e9103364ed05cc0c1b6
AbhiSawant99 <sawantabhijeet9990@gmail.com>
2025-08-31 18:53:31 +0530

    fix: fix for production cookie

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -29,7 +29,9 @@ export const authLogin = catchAsync(async (req: Request, res: Response) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in ms
   });
 
   res.status(httpStatus.OK).json({
